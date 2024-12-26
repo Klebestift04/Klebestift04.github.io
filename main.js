0152edfe05860a5293c97d7e551c0906df73ffaa
Klebestift04 <kamoune196@gmail.com>
2024-12-26 19:24:16 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Read more functiom
+
+let readMore = document.getElementById("read-more");
+
+function showInfo(){
+    moreInfo.style.display = 'block';
+
+}
+
+function hideInfo() {
+    hideInfo.style.display = ' none';
+}
+
+fucntion //share article
+
+
+// 
